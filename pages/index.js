d09e232103478245a441fcd8c3e7d16edd56bca1
Den <52295780+josserden@users.noreply.github.com>
2022-09-21 21:30:59 +0300

    add: base config on project

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,35 @@
 import Head from 'next/head';
-import { Component } from 'react';
+import Script from 'next/script';
+
 import { attributes, react as HomeContent } from '../content/home.md';
 
-export default class Home extends Component {
-  render() {
-    let { title, cats } = attributes;
-    return (
-      <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
+const Home = () => {
+  let { title, cats } = attributes;
 
-        <article>
-          <h1>{title}</h1>
+  return (
+    <>
+      <Head>
+        <title>Home Page</title>
+      </Head>
 
-          <HomeContent />
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
 
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
-        </article>
-      </>
-    );
-  }
-}
+      <article>
+        <h1>{title}</h1>
+
+        <HomeContent />
+
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  mode: 'jit', // see https://tailwindcss.com/docs/just-in-time-mode
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          md: '2rem',
+          lg: '2rem',
+          xl: '1.4rem',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography', require('@tailwindcss/forms'))],
+};
