140769fa891eb94fdccc1ceceb0b06fa3aa206f7
Svitlana Zhovanik <LanaSvetCat@gmail.com>
2022-09-22 16:06:04 +0300

    fix: added cards pages, test min style for markdown

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  webpack: (cfg) => {
+  webpack: cfg => {
     cfg.module.rules.push({
       test: /\.md$/,
       loader: 'frontmatter-markdown-loader',
       options: { mode: ['react-component'] },
     });
+    cfg.resolve.fallback = { fs: false, path: false };
     return cfg;
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head';
+import Script from 'next/script';
+import Link from 'next/link';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
 import { attributes, react as HomeContent } from '../content/home.md';
 
-const Home = () => {
+const Home = ({ slugs }) => {
   let { title, cats } = attributes;
-
   return (
     <>
       <Head>
         <title>Home Page</title>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
       </Head>
 
       <article>
@@ -25,9 +29,34 @@ const Home = () => {
             </li>
           ))}
         </ul>
+        <h2 className="mt-12 text-center font-bold">Ссылки</h2>
+        <ul className=" list-inside list-disc text-center">
+          {slugs.map((slug, idx) => (
+            <li key={idx}>
+              <Link href={`/${slug.href}`}>
+                <a>{slug.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </article>
     </>
   );
+};
+export const getStaticProps = async () => {
+  const files = fs.readdirSync('content/cards');
+  const slugs = files.map(filename => {
+    const href = filename.replace('.md', '');
+    const title = matter(fs.readFileSync(path.join('content/cards/', filename)))
+      .data.title;
+    return { title, href };
+  });
+
+  return {
+    props: {
+      slugs,
+    },
+  };
 };
 
 export default Home;
