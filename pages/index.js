bbd3f24fd4bce2b279a55b0f6e77e9541a09fbf2
Den <52295780+josserden@users.noreply.github.com>
2022-09-22 10:17:47 +0300

    add: base config on project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 
 import { attributes, react as HomeContent } from '../content/home.md';
 
@@ -10,9 +9,8 @@ const Home = () => {
     <>
       <Head>
         <title>Home Page</title>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
-
-      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
 
       <article>
         <h1>{title}</h1>
