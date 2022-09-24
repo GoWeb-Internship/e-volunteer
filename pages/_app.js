478278a2cfac4906dd35d564c53c7d883888a29a
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-09-24 18:03:00 +0300

    language

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/index.css';
 import { appWithTranslation } from 'next-i18next';
+import Header from "@/components/Header";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return( 
+    <>
+  <Header/>
+  <Component {...pageProps} />
+  </>
+  )
 };
 
 export default appWithTranslation(MyApp);
