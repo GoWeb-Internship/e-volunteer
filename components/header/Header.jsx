31d2ad3880bf1aaa4cd5b51b16445b8abf7b1124
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-09-28 18:42:05 +0300

    add

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { Search } from '@/components/Search/Search';
+import { Search } from '@/components/search/Search';
 import { useTranslation } from 'next-i18next';
 import Logo  from '../Logo/Logo';
 
