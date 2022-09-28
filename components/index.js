ab8e7c8882f7ced6a444d7a2e36df8393262a7c5
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-09-28 21:46:59 +0300

    add

--- components/index.js
+++ components/index.js
@@ -3,6 +3,6 @@ export * from './Grid/Grid';
 export * from './Search/Search';
 export * from './Form/Form';
 export * from './Logo/Logo';
-export * from './Header';
-export * from './Footer';
+export * from './Header/Header';
+export * from './Footer/Footer';
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/index.css';
 import { appWithTranslation } from 'next-i18next';
 import { Header, Footer } from '@/components';
 
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
