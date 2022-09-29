97934142e88e3a13d68083505be721f031028b90
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-09-29 08:47:50 +0300

    Update Logo.jsx

---
+++ components/Logo/Logo.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import React from 'react';
+import   LogoSite  from '../../public/img/Logo.svg';
+
+
+export const Logo = () => {
+
+    
+    return (
+        <Link href="/">
+        <a href="" aria-label="логотип">
+          <LogoSite className="w-[64px]" />
+        </a>
+      </Link>
+    );
+  }
+  
+
+Logo.propTypes = {};
